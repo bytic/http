143038d552ca8d1633efbab50114b6e8fb98cb8e
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-07 12:45:39 +0200

    update to middleland dispatcher

--- src/ServerMiddleware/DispatcherInterface.php
+++ src/ServerMiddleware/DispatcherInterface.php
@@ -3,11 +3,12 @@
 namespace Nip\Http\ServerMiddleware;
 
 use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 /**
  * Interface DispatcherInterface
  * @package Nip\Http\ServerMiddleware
  */
-interface DispatcherInterface extends MiddlewareInterface
+interface DispatcherInterface extends MiddlewareInterface, RequestHandlerInterface
 {
 }
